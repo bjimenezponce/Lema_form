13e7e5e9bda6e44637f6ff47a0c3a2c4906597f6
bjimenezponce <bjimenezponce1@gmail.com>
2024-07-25 09:34:47 -0400

    Form_prueba

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+//import "./App.css";
+import NavBar from "./components/ui/navbar";
+import Input from "./components/ui/input";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Input />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ui/input.tsx
@@ -0,0 +1,58 @@
+interface props extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+function Input(_props: props) {
+  return (
+    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form action="#" method="POST" className="space-y-6">
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Ingresa tu Email
+          </label>
+          <div className="mt-2">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              required
+              autoComplete="email"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <div className="flex items-center justify-between">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Password
+            </label>
+            <div className="text-sm">
+              <a
+                href="#"
+                className="font-semibold text-indigo-600 hover:text-indigo-500"
+              >
+                Olvidaste tu contraseña?
+              </a>
+            </div>
+          </div>
+          <div className="mt-2">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              required
+              autoComplete="current-password"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+export default Input;
